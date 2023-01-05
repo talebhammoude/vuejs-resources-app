41f0e2f643c447b083e3bd978476a71f187dadf5
Taleb Hammoudé <taleb.hammoude@outlook.com>
2023-01-05 22:38:38 +0100

    strukturändring

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
 <template>
-    <ul>
-        <LearningResource 
-        v-for="res in storedResources" 
-        :key="res.id" 
-        :title="res.title" 
-        :description="res.description" 
-        :link="res.link"
-        />
-    </ul>
+    <StoredResources :resources="storedResources"/>
 </template>
 
 <script>
-import LearningResource from './components/learning-resources/LearningResource.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue';
 
     export default {
         components: {
-            LearningResource
+            StoredResources
         },
 
         data() {
