aca6022f74689b0bc1cd0a474b15ed9202471477
Taleb Hammoudé <taleb.hammoude@outlook.com>
2023-01-09 20:53:42 +0100

    adding style to tab if selected

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -1,7 +1,7 @@
 <template>
     <base-card>
-        <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
-        <base-button @click="setSelectedTab('add-resource')">Add Resource</base-button>
+        <base-button @click="setSelectedTab('stored-resources')" :style="selectedTab === 'stored-resources' ? 'border-style:solid; border-width:2px; border-color: #640032;':null ">Stored Resources</base-button>
+        <base-button @click="setSelectedTab('add-resource')" :style="selectedTab === 'add-resource' ? 'border-style:solid; border-width:2px; border-color: #640032;':null ">Add Resource</base-button>
     </base-card>
     <component :is="selectedTab"></component>
 </template>
